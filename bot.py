810fe3418db56b72ff6e5b98186de59de015f3fd
dzienskaj <kamilgol1@gmail.com>
2018-02-23 00:03:45 +0100

    first commit

--- bot.py
+++ bot.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
-import redis
 import os
-import telebot
-# import some_api_lib
-# import ...
+from telegram.ext import Updater, MessageHandler, Filters
 
 # Example of your code beginning
 #           Config vars
 token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
-
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
 
 #       Your bot code below
 # bot = telebot.TeleBot(token)
 # some_api = some_api_lib.connect(some_api_token)
 #              ...
+
+updater = Updater(token=token)
+dispatcher = updater.dispatcher
+
+
+def replay_in_caps(bot, update):
+    message = update.message
+
+    if message.text == '?' and message.reply_to_message is not None:
+        bot.send_message(chat_id=message.chat_id,
+                         text=message.reply_to_message.text.upper(),
+                         reply_to_message_id=message.message_id)
+
+
+replay_in_caps_handler = MessageHandler(Filters.text, replay_in_caps)
+dispatcher.add_handler(replay_in_caps_handler)
+
+updater.start_polling()
